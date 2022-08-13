e85c03196ee749f3b8c3011b63ba22c2d703d6bd
AndrewSimons3 <simdrew07@gmail.com>
2022-08-13 13:06:35 -0700

    finished

--- script.js
+++ script.js
@@ -1,2 +1,62 @@
-'use strict'
+'use strict';
 
+const fetchExpensesData = async () => {
+	const response = await fetch('./data.json');
+	const data = await response.json();
+	console.log(data);
+};
+
+fetchExpensesData();
+
+const ctx = document.getElementById('myChart').getContext('2d');
+
+const myChart = new Chart(ctx, {
+	type: 'bar',
+	data: {
+		labels: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
+		datasets: [
+			{
+				data: [17.45, 34.91, 52.36, 31.07, 23.39, 43.28,25.48],
+
+				backgroundColor: [
+					'hsl(10, 79%, 65%)',
+					'hsl(10, 79%, 65%)',
+					'hsl(10, 79%, 65%)',
+					'hsl(10, 79%, 65%)',
+					'hsl(10, 79%, 65%)',
+					'hsl(10, 79%, 65%)',
+					'hsl(10, 79%, 65%)',
+				],
+
+        borderWidth: 0,
+        borderRadius: 5,
+				hoverBackgroundColor: ['hsl(186, 34%, 60%)'],
+			},
+		],
+	},
+	options: {
+		onClick: this.ClickHandler,
+		plugins: {
+			legend: {
+				display: false,
+			},
+		},
+		scales: {
+			y: {
+				ticks: {
+					display: false,
+				},
+				grid: {
+					drawBorder: false,
+					display: false,
+				},
+			},
+			x: {
+				grid: {
+					drawBorder: false,
+					display: false,
+				},
+			},
+		},
+	},
+});
